8c18e557c4a41094caa300fe11fad8e7288bde31
krakatit <polivkajan@yahoo.com>
2018-11-29 23:28:22 +0100

    First data creation

---
+++ 10_LoadData.R
@@ -0,0 +1,7 @@
+library("data.table")
+#creating a dummy data table
+DT <- data.table( ID = 1:50,
+                  Capacity = sample(100:1000, size = 50, replace = F),
+                  Code = sample(LETTERS[1:4], 50, replace = T),
+                  State = rep(c("Alabama","Indiana","Texas","Nevada"), 50))
+head(DT)
