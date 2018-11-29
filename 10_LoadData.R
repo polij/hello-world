d1f17f7a9366e74779024fbab28ac97135ffcd62
krakatit <polivkajan@yahoo.com>
2018-11-30 00:42:17 +0100

    Adding one more line

--- 10_LoadData.R
+++ 10_LoadData.R
@@ -5,3 +5,5 @@ DT <- data.table( ID = 1:50,
                   Code = sample(LETTERS[1:4], 50, replace = T),
                   State = rep(c("Alabama","Indiana","Texas","Nevada"), 50))
 head(DT)
+#the added line
+#one more line
